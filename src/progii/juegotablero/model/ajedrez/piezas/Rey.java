f8138a3362ce8c58b3a2d2731a89103bb950db93
IA IA AA YE YE YE YE <alejandro.gallegogragera@alumnos.upm.es>
2026-05-15 20:05:07 +0200

    Refactor Rey movements to include all directions
    Updated the valid movements for the 'Rey' piece to include all possible directions.

--- src/progii/juegotablero/model/ajedrez/piezas/Rey.java
+++ src/progii/juegotablero/model/ajedrez/piezas/Rey.java
@@ -1,5 +1,6 @@
 package progii.juegotablero.model.ajedrez.piezas;
 
+import list.ArrayList;
 import list.IList;
 import progii.juegotablero.model.Casilla;
 import progii.juegotablero.model.Jugador;
@@ -10,12 +11,32 @@ public class Rey extends PiezaAjedrez {
 
 	@Override
 	public IList<Casilla> movimientosValidos() {
-		IList<Casilla> resultado = new IList <>();
-		casillaVisitable(resultado, -1,1);
+		IList<Casilla> resultado = new ArrayList<>();
+		
+		// Avanzar a la casilla superior de la que se sitúa el rey.
+		casillaVisitable(resultado,0,-1);
+		
+		// Avanzar a la casilla diagonal derecha de la que se sitúa el rey.
+		casillaVisitable(resultado,1,-1);
+		
+		// Avanzar a la casilla de la derecha de la que se sitúa el rey.
 		casillaVisitable(resultado,1,0);
-		casillaVisitable(resultado, -1,-1);
-		casillaVisitable(resultado, 0,1);
-		casillaVisitable(resultado, -1,0);
+		
+		// Avanzar a la casilla diagonal inferior derecha de la que se sitúa el rey.
+		casillaVisitable(resultado,1,1);
+		
+		// Avanzar a la casilla inferior de la que se sitúa el rey.
+		casillaVisitable(resultado,0,1);
+		
+		// Avanzar a la casilla diagonal inferior izquierda de la que se sitúa el rey.
+		casillaVisitable(resultado,-1,1);
+		
+		// Avanzar a la casilla de la izquierda de la que se sitúa el rey.
+		casillaVisitable(resultado,-1,0);
+		
+		// Avanzar a la casilla diagonal superior izquierda de la que se encuentra el rey.
+		casillaVisitable(resultado,-1,-1);
+		
 		return resultado;
 	}
 	public Rey (Jugador jugador, int fila, char columna) {
