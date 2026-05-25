1f2d01db89e67927070506ff60f752f07d66d158
IA IA AA YE YE YE YE <alejandro.gallegogragera@alumnos.upm.es>
2026-05-25 20:52:32 +0200

    Refactor Rey class to use current position for moves

--- src/progii/juegotablero/model/ajedrez/piezas/Rey.java
+++ src/progii/juegotablero/model/ajedrez/piezas/Rey.java
@@ -1,3 +1,8 @@
+/*
+ * Autor: Alejandro Gallego Gragera.
+ * Correo electrónico: alejandro.gallegogragera@alumnos.upm.es
+ */
+
 package progii.juegotablero.model.ajedrez.piezas;
 
 import list.ArrayList;
@@ -12,31 +17,33 @@ public class Rey extends PiezaAjedrez {
 	@Override
 	public IList<Casilla> movimientosValidos() {
 		IList<Casilla> resultado = new ArrayList<>();
-		
+		int filaActual = super.getFila();
+		int columnaActual = super.getColumna();
+
 		// Avanzar a la casilla superior de la que se sitúa el rey.
-		casillaVisitable(resultado,0,-1);
-		
+		casillaVisitable(resultado,filaActual,columnaActual-1);
+
 		// Avanzar a la casilla diagonal derecha de la que se sitúa el rey.
-		casillaVisitable(resultado,1,-1);
-		
+		casillaVisitable(resultado,filaActual+1,columnaActual-1);
+
 		// Avanzar a la casilla de la derecha de la que se sitúa el rey.
-		casillaVisitable(resultado,1,0);
-		
+		casillaVisitable(resultado,filaActual+1,columnaActual);
+
 		// Avanzar a la casilla diagonal inferior derecha de la que se sitúa el rey.
-		casillaVisitable(resultado,1,1);
-		
+		casillaVisitable(resultado,filaActual+1,columnaActual+1);
+
 		// Avanzar a la casilla inferior de la que se sitúa el rey.
-		casillaVisitable(resultado,0,1);
-		
+		casillaVisitable(resultado,filaActual,columnaActual+1);
+
 		// Avanzar a la casilla diagonal inferior izquierda de la que se sitúa el rey.
-		casillaVisitable(resultado,-1,1);
-		
+		casillaVisitable(resultado,filaActual-1,columnaActual+1);
+
 		// Avanzar a la casilla de la izquierda de la que se sitúa el rey.
-		casillaVisitable(resultado,-1,0);
-		
+		casillaVisitable(resultado,filaActual-1,columnaActual);
+
 		// Avanzar a la casilla diagonal superior izquierda de la que se encuentra el rey.
-		casillaVisitable(resultado,-1,-1);
-		
+		casillaVisitable(resultado,filaActual-1,columnaActual-1);
+
 		return resultado;
 	}
 	public Rey (Jugador jugador, int fila, char columna) {
