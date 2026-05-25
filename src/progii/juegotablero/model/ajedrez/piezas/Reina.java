e9737e12fcfddb79628771d002d2f86522d64f4a
IA IA AA YE YE YE YE <alejandro.gallegogragera@alumnos.upm.es>
2026-05-25 20:52:01 +0200

    Refactor Reina class with added author comment

--- src/progii/juegotablero/model/ajedrez/piezas/Reina.java
+++ src/progii/juegotablero/model/ajedrez/piezas/Reina.java
@@ -1,4 +1,8 @@
-ackage progii.juegotablero.model.ajedrez.piezas;
+/*
+ * Autor: Alejandro Gallego Gragera.
+ * Correo electrónico: alejandro.gallegogragera@alumnos.upm.es
+ */
+package progii.juegotablero.model.ajedrez.piezas;
 
 import list.ArrayList;
 import list.IList;
@@ -12,23 +16,23 @@ public class Reina extends PiezaAjedrez {
 	@Override
 	public IList<Casilla> movimientosValidos() {
 		IList<Casilla> resultado = new ArrayList<>();
-		
+
 		//Vertical descendente según el modelo interno (matriz)
 		casillasVisitables (resultado, 1, 0);
 		casillasVisitables (resultado, 1,1);
-		
+
 		//Vertical ascendente según el modelo interno (matriz)
 		casillasVisitables (resultado, -1, 0);
 		casillasVisitables (resultado, -1,1);
 
-		//Horiznotal izquierda según el modelo interno (matriz)
+		//Horizontal izquierda según el modelo interno (matriz)
 		casillasVisitables (resultado, 0, -1);
 		casillasVisitables (resultado, 1,-1);
-		
+
 		//Horizontal derecha según el modelo interno (matriz)
 		casillasVisitables (resultado, 0, 1);
 		casillasVisitables (resultado, -1,-1);
-		
+
 
 		return resultado;
 	}
