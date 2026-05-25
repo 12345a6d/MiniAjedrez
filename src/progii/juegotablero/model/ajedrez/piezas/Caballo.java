a3dce7def1580667fb7cb1c3de9f0d3c4d2f930b
IA IA AA YE YE YE YE <alejandro.gallegogragera@alumnos.upm.es>
2026-05-25 20:50:41 +0200

    Refactor Caballo movimientosValidos to use current position
    Updated the Caballo class to use current position for valid moves.

--- src/progii/juegotablero/model/ajedrez/piezas/Caballo.java
+++ src/progii/juegotablero/model/ajedrez/piezas/Caballo.java
@@ -1,3 +1,8 @@
+/*
+ * Autor: Alejandro Gallego Gragera.
+ * Correo electrónico: alejandro.gallegogragera@alumnos.upm.es
+ */
+
 package progii.juegotablero.model.ajedrez.piezas;
 
 import list.ArrayList;
@@ -12,33 +17,34 @@ public class Caballo extends PiezaAjedrez {
 	@Override
 	public IList<Casilla> movimientosValidos() {
 		IList<Casilla> resultado = new ArrayList<>();
-		
+		int filaActual = super.getFila();
+		int columnaActual = super.getColumna();
 		//Describimos el caballo como un movimiento en L, así que incFila = 2 (si el movimiento es así ¬) o incColumna = 2 (si el movimiento es una L invertida).
-		
+
 		//El caballo se mueve dos casillas en horizontal izquierda y una casilla hacia arriba.
-		casillaVisitable(resultado,-1,-2);
-		
+		casillaVisitable(resultado,filaActual-1, columnaActual-2);
+
 		//El caballo se mueve dos casillas en horizontal izquierda y una casilla hacia abajo.
-		casillaVisitable(resultado,1 ,-2);
-		
+		casillaVisitable(resultado,filaActual+1 , columnaActual-2);
+
 		//El caballo se mueve dos casillas en vertical hacia arriba y una casilla a la derecha.
-		casillaVisitable(resultado, -2 ,1);
-		
+		casillaVisitable(resultado, filaActual-2 ,columnaActual +1);
+
 		//El caballo se mueve dos casillas en vertical hacia arriba y una casilla a la izquierda.
-		casillaVisitable(resultado,-2,-1);
-		
+		casillaVisitable(resultado,filaActual-2,columnaActual-1);
+
 		//El caballo se mueve dos casillas en horizontal derecha y una hacia arriba.
-		casillaVisitable(resultado,-1,2);
-		
+		casillaVisitable(resultado,filaActual-1,columnaActual+2);
+
 		//El caballo se mueve dos casillas en horizontal derecha y una hacia abajo.
-		casillaVisitable(resultado, 1, 2);
-		
+		casillaVisitable(resultado, filaActual+1, columnaActual+2);
+
 		//El caballo se mueve dos casillas en vertical hacia abajo y una casilla a la derecha.
-		casillaVisitable(resultado, 2,1);
-		
+		casillaVisitable(resultado, filaActual+2,columnaActual+1);
+
 		// El caballo se mueve dos casillas en vertical hacia abajo y una casilla a la izquierda.
-		casillaVisitable(resultado,2,-1);
-		
+		casillaVisitable(resultado,filaActual+2,columnaActual-1);
+
 		return resultado;
 	}
 	public Caballo (Jugador jugador, int fila, char columna) {
